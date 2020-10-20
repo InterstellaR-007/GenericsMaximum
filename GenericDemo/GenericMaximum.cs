23687dfe0379647ed54a3ac06f65ae8c4cf07da6
InterstellaR-007 <intrstelr.007@gmail.com>
2020-10-20 19:57:05 +0530

    Master Branch

---
+++ GenericDemo/GenericMaximum.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericDemo
+{
+    public class GenericMaximum<T> where T:IComparable
+    {
+        public T[] input_Values;
+
+        public GenericMaximum(T[] input_Values)
+        {
+            this.input_Values = input_Values;
+        }
+
+        public T[] Sort(T[] input_Values)
+        {
+            Array.Sort(input_Values);
+            return input_Values;
+        }
+
+        public T getMaximum(T[] input_Values)
+        {
+            var sorted_Values = Sort(input_Values);
+            return sorted_Values[^1];
+
+        }
+        public T MaximumValue()
+        {
+            var max = getMaximum(this.input_Values);
+            return max;
+        }
+        public void printMaxValue()
+        {
+            var max = MaximumValue();
+            Console.WriteLine("Maximum values is :" + max);
+        }
+    }
+}
---
+++ GenericDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace GenericDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] intArray = { 1, 2, 3, 4, 5 };
+            double[] doubleArray = { 1.1, 2.2, 3.3, 4.4, 5.5 };
+            string[] stringArray = { "1.22","2.54","10.2222" };
+
+            GenericMaximum<int> getMaximumInteger = new GenericMaximum<int>(intArray);
+            getMaximumInteger.printMaxValue();
+
+            GenericMaximum<string> getMaximumString = new GenericMaximum<string>(stringArray);
+            getMaximumString.printMaxValue();
+
+            GenericMaximum<double> getMaximumDouble = new GenericMaximum<double>(doubleArray);
+            getMaximumDouble.printMaxValue();
+
+        }
+        
+        
+    }
+}
